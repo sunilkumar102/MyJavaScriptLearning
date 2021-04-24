6189d9e7cdc1edf98f4254f3fed6404a8f554eb6
sunilkumar102 <sunilkumarkumawat102@gmail.com>
2021-04-24 21:19:08 +0530

    Ïnitial commit

---
+++ starter/assignment.js
@@ -0,0 +1,19 @@
+// let country = "India";
+//'let continent = "Asia";
+//'let population = "12100000";
+//'console.log(country);
+//'console.log(continent);
+//'console.log(population);
+
+//'let myNameIsSunil = true;
+//'console.log(typeof myNameIsSunil);
+//'myNameIsSunil = "Sunil"
+//'console.log(typeof myNameIsSunil);
+const birthyear = 1998;
+console.log(birthyear, 2 ^ 3);
+let x = 10 + 4;
+x -= 2;
+x *= 3;
+console.log(x);
+const y = 8;
+console.log(x < y);
---
+++ starter/script.js
@@ -0,0 +1,2 @@
+let firstName = "Sunil";
+console.log(firstName);
